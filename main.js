f747ee8aa0d19381b0b14133158ce036bbea45c1
haley.morgan <haleymorgan3264@gmail.com>
2019-07-02 14:11:22 -0400

    add new pics to photo gallery

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
-var displayedImage = document.querySelector('.displayed-img');
-var thumbBar = document.querySelector('.thumb-bar');
+let displayedImage = document.querySelector('.displayed-img');
+let thumbBar = document.querySelector('.thumb-bar');
 
-var btn = document.querySelector('button');
-var overlay = document.querySelector('.overlay');
+let btn = document.querySelector('button');
+let overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+for(let i = 1; i <= 5; i++) {
+  let newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    let imgsrc = e.target.getAttribute('src');
+    displayImage(imgsrc)
+  }
+}
+
+// display the clicked image 
+
+function displayImage(imgsrc) {
+  displayedImage.setAttribute('src', imgsrc);
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
+
+
+
